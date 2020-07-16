1448df36ae290722459afa9e9d890387cb824bae
NAIRA52 <63794699+NAIRA52@users.noreply.github.com>
2020-07-16 19:56:14 +0200

    construction de la page produit

--- liste.js
+++ liste.js
@@ -1,5 +1,5 @@
 // Affiche les noeuds enfant du noeud body
-for (var i = 0; i < document.body.childNodes.length; i++) {
+for (let i = 0; i < document.body.childNodes.length; i++) {
     console.log(document.body.childNodes[i]);
 }
 
@@ -11,25 +11,27 @@ const prenom = prompt("Entrez votre prénom :");
 alert(`Bonjour, ${prenom}`);
 
 //creation de la liste de produit
-function Products(image, name, price, description) {
+function Products(_id, image, name, price, description, lien) {
+    this._id = _id;
     this.image = image;
     this.name = name;
     this.price = price;
     this.description = description;
+    this.lien = lien;
 }
 
-const teddy_1 = new Products('http://localhost:3000/images/teddy_1.jpg', 'Norbert', 2900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-const teddy_2 = new Products('http://localhost:3000/images/teddy_2.jpg', 'Arnold', 3900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-const teddy_3 = new Products('http://localhost:3000/images/teddy_3.jpg', 'Lenny and Carl', 5900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-const teddy_4 = new Products('http://localhost:3000/images/teddy_4.jpg', 'Gustav', 4500, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-const teddy_5 = new Products('http://localhost:3000/images/teddy_5.jpg', 'Garfunkel', 5500, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+const teddy_1 = new Products('5be9c8541c9d440000665243', 'http://localhost:3000/images/teddy_1.jpg', 'Norbert', 2900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'produit.html#Block1');
+const teddy_2 = new Products('5beaa8bf1c9d440000a57d94', 'http://localhost:3000/images/teddy_2.jpg', 'Arnold', 3900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'produit.html#Block2');
+const teddy_3 = new Products('5beaaa8f1c9d440000a57d95', 'http://localhost:3000/images/teddy_3.jpg', 'Lenny and Carl', 5900, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'produit.html#Block3');
+const teddy_4 = new Products('5beaabe91c9d440000a57d96', 'http://localhost:3000/images/teddy_4.jpg', 'Gustav', 4500, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'produit.html#Block4');
+const teddy_5 = new Products('5beaacd41c9d440000a57d97', 'http://localhost:3000/images/teddy_5.jpg', 'Garfunkel', 5500, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'produit.html#Block5');
 
 console.log(teddy_1, teddy_2, teddy_3, teddy_4, teddy_5);
 
 let products = [];
 products.push(teddy_1, teddy_2, teddy_3, teddy_4, teddy_5);
 
-function populateTableList() {
+function TableList() {
 
     let listOfProcuts = "";
 
@@ -40,12 +42,10 @@ function populateTableList() {
         <td class="w-25 align-middle">${prod.name}</td>
         <td class="w-25 align-middle">${prod.price}€</td>
         <td class="w-25 align-middle">${prod.description}</td>
-        <td class="w-25 align-middle"><button class="btn btn-info" id="button">Voir</button></td>
+        <td class="w-25 align-middle"><a href="${prod.lien}" class= "btn btn-info" >Voir</a></td>
       </tr>
       `
     )
 
     document.getElementById('productList').innerHTML = listOfProcuts;
 }
-
-// personnalisation du produit//
---
+++ produit.js
@@ -0,0 +1,75 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://naira52.github.io/Mon-projet-5/");
+request.send();
+
+// affichage de "des blocks" sur la console
+console.log("liste des produits");
+
+function Blocks(image, name, price, description, lien, color1, color2, color3, color4) {
+    this.image = image;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.lien = lien;
+    this.color1 = color1;
+    this.color2 = color2;
+    this.color3 = color3;
+    this.color4 = color4;
+
+}
+
+const teddy_1 = new Blocks('http://localhost:3000/images/teddy_1.jpg', 'Norbert', '2900 €', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Block1', 'Tan', 'Chocolate', 'Black', 'White');
+const teddy_2 = new Blocks('http://localhost:3000/images/teddy_2.jpg', 'Arnold', '3900 €', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Block2', 'Pale brown', 'Dark brown', 'White');
+const teddy_3 = new Blocks('http://localhost:3000/images/teddy_3.jpg', 'Lenny and Carl', '5900 €', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Block3', 'Brown');
+const teddy_4 = new Blocks('http://localhost:3000/images/teddy_4.jpg', 'Gustav', '4500 €', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Block4', 'Brown', 'Blue', 'Pink');
+const teddy_5 = new Blocks('http://localhost:3000/images/teddy_5.jpg', 'Garfunkel', '5500 €', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Block5', 'Beige', 'Tan', 'Chocolate');
+
+console.log(teddy_1, teddy_2, teddy_3, teddy_4, teddy_5);
+
+//description du Block1
+let blocks = [];
+blocks.push(teddy_1, teddy_2, teddy_3, teddy_4, teddy_5); {
+    let ContBlock = "";
+    blocks.forEach(bloc =>
+        ContBlock += `
+        <article class="container text-light m-5 bg-dark" id=${bloc.lien}>
+        <div class="row p-4">
+        <div class="col-md-6 mb-4">
+            <img src=${bloc.image} alt="Norbert" style="width: 100%;"></img>
+        </div>
+
+        <div class="col-md-6 mb-4">
+            <div class="p-4">
+                <h2>${bloc.name}</h2>
+                <h3 class="lead font-weight-bold">${bloc.price}</h3>
+                <h4 class="text-danger font-weight-bold">Description</h4>
+                <h5>${bloc.description}</h5>
+                <h6 class="text-success">En stock</h6>
+            </div>
+
+            <form class="d-flex justify-content-around">
+                <input type="number" value="0" min="0" aria-label="Search" class="form-control" style="width: 100px">
+
+                <select class="md-form mdb-select colorful-select dropdown-primary">
+                    <option value="" disabled selected>Choisir la couleur</option>
+                    <option value="1">${bloc.color1}</option>
+                    <option value="2">${bloc.color2}</option>
+                    <option value="3">${bloc.color3}</option>
+                    <option value="4">${bloc.color4}</option>
+                  </select>
+
+                <button class="btn btn-primary btn-md my-0 p" type="submit">Ajouter au panier
+                            <i class="fas fa-shopping-cart ml-1"></i>
+                            <a class="panier.html"></a>
+                     </button>
+
+            </form>
+        </div>
+    </div>
+    </article>
+
+    `
+
+    )
+    document.getElementById('Blok').innerHTML = ContBlock;
+}
